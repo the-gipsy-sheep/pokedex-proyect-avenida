ba349eddf366835e9dae0ac5299d8e0b123c7cbe
Demian Yovanovich <subwaydemiany@gmail.com>
2023-01-17 17:17:43 -0300

    se agrega la gema poke-api para realizar consultas

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,10 @@
 class PokemonsController < ApplicationController
   def index
-
+    @pokemons = []
+    10.times do
+      pokemon = PokeApi.get(pokemon: (1..1008).to_a.sample.to_s)
+      @pokemons << pokemon
+    end
   end
 
   def show
